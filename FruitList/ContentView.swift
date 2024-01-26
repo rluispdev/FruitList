25ac95ff509574936aec1543c4f70d0bd3b38c09
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-26 20:15:38 -0300

    Criar o conteudo da lista.

--- FruitList/ContentView.swift
+++ FruitList/ContentView.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var fruitArray: [FruitModel] = []
+    @State var fruitArray: [FruitModel] = [
+        
+    FruitModel(name: "Apples", count: 5)
+    
+    ]
     
     var body: some View {
         NavigationStack{
             List{
                 ForEach(fruitArray) { fruit in
                     HStack {
+                        Text("\(fruit.count)")
+                            .foregroundStyle(.pink)
                         Text(fruit.name)
+                            .font(.headline)
+                            .bold()
                     }
                 }
             }
