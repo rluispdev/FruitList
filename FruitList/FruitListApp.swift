a71a7e62706b9710942a79e2ab32e415615be97d
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-26 19:57:32 -0300

    Commit inicial

---
+++ FruitList/FruitListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitListApp.swift
+//  FruitList
+//
+//  Created by Rafael Gonzaga on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
